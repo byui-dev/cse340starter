723878b00fd6e1b59990d40f226d7a9b40c6c7cb
byui-dev <okdiphala@hotmail.com>
2026-01-30 18:57:26 +0200

    refactor inventory-model.js to use the correct column name for the inventory table based on the name taken from the database schema

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -34,7 +34,7 @@ async function getVehicleById(inventory_id) {
         `SELECT * FROM public.inventory AS i
         JOIN public.classification AS c 
         ON i.classification_id = c.classification_id
-        WHERE i.inventory_id = $1`,
+        WHERE i.inv_id = $1`,
         [inventory_id]
         )
         return data.rows[0] // single vehicle

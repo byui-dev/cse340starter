62aa2ffd1575e9f533f5dd047434a5432047f613
byui-dev <okdiphala@hotmail.com>
2026-02-11 16:22:00 +0200

    add cookieParser to server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const store = require("connect-pg-simple")(session)
 const pool = require("./database/")
 const accountRoute = require("./routes/accountRoute")
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
 /****************************
  * Middleware
@@ -44,6 +45,9 @@ app.use(function(req, res, next){
 // Body Parser Middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))  // for parsing application/x-www-form-urlencoded
+
+app.use(cookieParser())
+
 
 /* ***********************
  * View Engine and Templates
